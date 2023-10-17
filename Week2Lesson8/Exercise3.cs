00684180ae58a595a796105ee702781073b53f97
Radosław Falak <radoslaw.falak@gmail.com>
2023-10-17 22:02:17 +0200

    add Exercise3 with method Fibbonaci

---
+++ Week2Lesson8/Exercise3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Lesson8
+{
+    internal class Exercise3
+    {
+        public static void Fibonacci()
+        {
+            int a = -1;
+            int b = 1;
+            int c = 0;
+            for (int i = 0; i < 13; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+                Console.WriteLine(b); // 0,1,1,2,3,5,8,13,21,34,55,89,144   
+            }           
+        }
+    }
+}
--- Week2Lesson8/Program.cs
+++ Week2Lesson8/Program.cs
@@ -4,6 +4,6 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Exercise2.EvenNumbers();
+        Exercise3.Fibonacci();
     }
 }
