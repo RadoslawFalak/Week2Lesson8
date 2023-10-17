528fefeed56f4b1c27f70adaa33376140c591820
Radosław Falak <radoslaw.falak@gmail.com>
2023-10-17 20:20:12 +0200

    Add project files.

---
+++ Week2Lesson8/Exercise1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Lesson8
+{
+    internal class Exercise1
+    {
+        public static void HowManyNumbers()
+        {
+            int a = 0;
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i > 1 && i %1 == 0 && i %i == 0)
+                {
+                    a++;
+                }             
+            }
+            Console.WriteLine(a);
+        }
+    }
+}
---
+++ Week2Lesson8/Program.cs
@@ -0,0 +1,9 @@
+﻿using Week2Lesson8;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Exercise1.HowManyNumbers();
+    }
+}
