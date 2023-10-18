2b01a9d731144b11035968c19fcb58180c5102aa
Radosław Falak <radoslaw.falak@gmail.com>
2023-10-18 22:45:40 +0200

    add Exercise6 with method Fractions

---
+++ Week2Lesson8/Exercise5.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Lesson8
+{
+    internal class Exercise5
+    {
+        public static void Exponentiation()
+        {
+            for ( int i = 1; i < 21; i++ )
+            {
+                Console.WriteLine( i + "^3 = " + Math.Pow(i, 3));
+            }
+        }
+    }
+}
---
+++ Week2Lesson8/Exercise6.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Lesson8
+{
+    internal class Exercise6
+    {
+        public static void Fractions()
+        {
+            double number = 0;
+            for (int i = 1; i <= 20; i++)
+            {               
+                number += 1.0 / i;              
+            }
+            Console.WriteLine(Math.Round(number, 3));
+        } 
+    }
+}
