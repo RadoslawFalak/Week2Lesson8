cd84e5c614b76da699c4da2d71ee41f4d38eab66
Radosław Falak <radoslaw.falak@gmail.com>
2023-10-21 11:27:02 +0200

    add Exercise7  with method Diamond

---
+++ Week2Lesson8/Exercise7.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Lesson8
+{
+    internal class Exercise7
+    {
+        public static void Diamond()
+        {
+            int userNumber;
+            string input = Console.ReadLine();
+            int.TryParse(input, out userNumber);
+
+            for (int i = 1; i <= userNumber; i++)
+            {               
+                for (int j = 1; j <= userNumber; j++)
+                {
+                   int leftSpace= (userNumber - i) / 2;
+                   int rightSpace = ((userNumber - i) / 2) + i;
+                    
+                    if (leftSpace >= j || rightSpace < j)
+                    {
+                        Console.Write("-");
+                    }
+                    else
+                    {
+                        Console.Write("*");
+                    }
+                    
+                }
+                Console.WriteLine("");
+            }
+            for (int i = userNumber - 1; i >= 1; i--)
+            {
+                for (int j = 1; j <= userNumber; j++)
+                {
+                    int leftSpace = (userNumber - i) / 2;
+                    int rightSpace = ((userNumber - i) / 2) + i;
+
+                    if (leftSpace >= j || rightSpace < j)
+                    {
+                        Console.Write("-");
+                    }
+                    else
+                    {
+                        Console.Write("*");
+                    }
+
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
+/*---*---
+  --***--
+  -*****-
+  ********/
--- Week2Lesson8/Program.cs
+++ Week2Lesson8/Program.cs
@@ -4,6 +4,6 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Exercise6.Fractions();
+        Exercise7.Diamond();
     }
 }
