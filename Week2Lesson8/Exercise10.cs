407ccc6603d62651e0b094cc87be301a8a79b6b6
Radosław Falak <radoslaw.falak@gmail.com>
2023-10-25 22:25:50 +0200

    add class Exercise10

---
+++ Week2Lesson8/Exercise10.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Lesson8
+{
+    internal class Exercise10
+    {
+        static int LeastCommonDivisor(int a, int b)
+        {
+            while (a != b)
+            {
+                if (a < b)
+                {
+                    b -= a;
+                }
+                else
+                {
+                    a -= b;
+                }
+            }
+            return a;
+        }
+
+        public static void LeastCommonMultiple() 
+        {
+            int numberOne;
+            int numberTwo;
+            Console.WriteLine("Najmniejsza wspolna wielokrotnosc dwoch liczb.");
+            Console.Write("Podaj pierwsza liczbe: ");
+            string input = Console.ReadLine();
+            int.TryParse(input, out numberOne);
+            Console.Write("Podaj pierwsza liczbe: ");
+            input = Console.ReadLine();
+            int.TryParse(input, out numberTwo);
+            int leastCommonDivisor = Exercise10.LeastCommonDivisor(numberOne, numberTwo);
+            Console.WriteLine($"NWW({numberOne},{numberTwo}) = " + numberOne * numberTwo / leastCommonDivisor);
+        }
+    }
+}
--- Week2Lesson8/Program.cs
+++ Week2Lesson8/Program.cs
@@ -4,6 +4,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Exercise9.DecimalToBinary();
+        Exercise10.LeastCommonMultiple();
+        
     }
 }
